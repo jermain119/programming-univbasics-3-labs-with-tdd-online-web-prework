545e72edbd9ed7f21abde4632171c3004de3e7ad
joshua richards <jermain119@gmail.com>
2019-09-26 22:35:09 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,11 @@
+# assining intergers
 first_number=6
 second_number=2
+# assingining sum to first_number & second_number
 sum= first_number + second_number
+# assingining difference to first_number & second_number
 difference= first_number - second_number
+assingining product to first_number & second_number
 product= first_number * second_number
+assingining quotient to first_number & second_number
 quotient= first_number / second_number 
